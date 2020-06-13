34b27d0769e3ce5b1eab22b5974f502e6dc93c4b
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-13 13:48:26 -0400

    added an event listener for local storage and added content on high score html and js files. fixed html typo.

--- scores.js
+++ scores.js
@@ -1,1 +1,19 @@
-var high
+//declares variables
+var highscore = document.querySelector("#highscore");
+var clear= document.querySelector("#clear");
+//retieve scores from local storage
+var newScore = localStorage.getItem("newScore");
+newScore= JSON.parse(newScore);
+
+if (newScore !== null){
+    for (var i= 0; i < newScore.length; i++) {
+        var list = document.createElement("li");
+        list.textContent = newScore[i].initials + " "+ newScore[i].score
+        highscore.appendChild(list);
+    }
+}
+//event listener to clear score
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
